8f28e8453e0fd0a4dd9a774c212cca8cd4d2e8e5
Ivan Koshelev <ivan.koshelev@volvo.com>
2023-12-19 21:38:36 +0100

    Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var integers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+            Exensions.Predicate<int> predicate = IsSquare;
+
+            var squares = Exensions.Filter(integers, predicate);
+
+            Console.WriteLine(JsonSerializer.Serialize(squares));
+
+            var evenNumbers = Exensions.Filter(integers, delegate (int val) { return val % 2 == 0; });
+
+            Console.WriteLine(JsonSerializer.Serialize(evenNumbers));
+
+            var oddNumbers = Exensions.Filter(integers, (val) => val % 2 == 1);
+
+            Console.WriteLine(JsonSerializer.Serialize(oddNumbers));
+
+            // here we are using Filter as an "Extension method".
+            // It's the same as before, but the syntax is easier to read
+            var numbersDivisibleBy3 = integers.Filter((val) => val % 3 == 0);
+
+            Console.WriteLine(JsonSerializer.Serialize(numbersDivisibleBy3));
+
+            // finally, this is how we do it with built-in methods of C#:
+            Func<int, bool> predicate2 = (val) => val % 4 == 0;
+            var numbersDivisibleBy4 = integers.Where(predicate2).ToArray();
+            // or as inline
+            numbersDivisibleBy4 = integers.Where((val) => val % 4 == 0).ToArray();
+
+            Console.WriteLine(JsonSerializer.Serialize(numbersDivisibleBy4));
+        }
+
+        public static bool IsSquare(int val) => Math.Sqrt(val) % 1 == 0;
+    }
+
+    public static class Exensions
+    {
+        public delegate bool Predicate<T>(T obj);
+        public static T[] Filter<T>(this T[] source, Predicate<T> predicate)
+        {
+            var result = new List<T>();
+            foreach (var item in source) 
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ GarbageCollectorAndFinalization/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace GarbageCollectorAndFinalization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Make sure you are running in Release mode without debug breakpoints - " +
+                "they may interfiere with Garbage Collector.");
+
+            PrintCurrentUsedMemory();
+
+            var container = new Container();
+
+            var random = new Random(7);
+            for (int i = 0; i < 10 * 1024; i++) 
+            {
+                var buffer = new byte[1024];
+                random.NextBytes(buffer);
+                container.Content.Add(buffer);
+            }
+
+            PrintCurrentUsedMemory();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            PrintCurrentUsedMemory();
+
+            Container.StaticReference = container;
+
+            container = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            PrintCurrentUsedMemory();
+
+            Console.WriteLine($"container is currently in generation {GC.GetGeneration(Container.StaticReference)}");
+
+            Container.StaticReference = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            PrintCurrentUsedMemory(); // now object is gone
+        }
+
+        private static void PrintCurrentUsedMemory()
+        {
+            var usedDemoryAmount = GC.GetTotalMemory(true);
+            var usedMemoryDesription = BytesToString(usedDemoryAmount);
+            Console.WriteLine(usedMemoryDesription);
+        }
+
+        //https://stackoverflow.com/a/4975942
+        public static String BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+    }
+
+    public class Container
+    {
+        public static Container? StaticReference { get; set; }
+
+        public List<byte[]>? Content { get; set; } = new List<byte[]>();
+
+        ~Container()
+        {
+            System.Console.WriteLine("Finalizer firing.");
+        }
+    }
+
+
+}
