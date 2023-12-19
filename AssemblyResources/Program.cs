2e82744362d112836458d6055fae1dfc2269be6d
Ivan Koshelev <ivan.koshelev@volvo.com>
2023-12-19 17:46:14 +0100

    Assembly resources

---
+++ AssemblyResources/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace AssemblyResources
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            string resourceName = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith("Sample.txt"));
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
